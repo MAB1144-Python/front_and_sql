d2983f923c02a832c76b7da7a0b9f8566b3f4e33
Brayan Andru Montenegro Embus <75086331+MAB1144-Python@users.noreply.github.com>
2023-10-25 16:53:01 -0500

    incluite request

---
+++ backend/ejercicios2.jv/.js/ejercicios3.java
@@ -0,0 +1,12 @@
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        
+        int numero1 = 5;
+        int numero2 = 10;
+        
+        if (numero2 > numero1) {
+            System.out.println("num2 es mayor que num1");
+        }
+    }
+}
---
+++ backend/ejercicios2.jv/.js/newFile.ts
@@ -0,0 +1,13 @@
+{
+    public class variable {
+
+
+        public static void; main(String, [], args);
+    }
+
+    int; num1 = 5;
+    int; num2 = 10;
+
+    if (num1 > num2) {
+    }
+}
