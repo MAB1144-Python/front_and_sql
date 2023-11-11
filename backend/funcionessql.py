586e64cf8a68b3bbc63b3f257697948e68eb3273
mab <brayanm@0klg7.onmicrosoft.com>
2023-11-10 22:04:08 -0500

    add request sql

--- backend/funcionessql.py
+++ backend/funcionessql.py
@@ -39,3 +39,37 @@ def agregar_estudiante_sql(idusuario,numerodocumento,nombres,apellidos,semestre,
     conexion.close()
 #conexion.execute("insert into usuario (idusuario,numerodocumento,nombres,apellidos,semestre,jornada, idprograma,idtipousuario) values (?,?,?,?,?,?,?,?)", (1,147,'jhon','cano',None,None,1,1))
 #print(buscar_elementos())
+
+def listado_de_sala():
+    conexion=sqlite3.connect("control_de_modulo_salas.db")
+    # cursor=conexion.execute("select * from usuario WHERE numerodocumento= "+ str(documento))
+    df = pd.read_sql_query("SELECT descripcion from sala", conexion)
+    print(df)
+    #nos sirve para crear el id busca los id identificas cual es el mayor y le suma uno para garantizar que el id no existe
+    print(df["descripcion"]==0)
+    return df["descripcion"].values
+
+def generar_idprestamo():
+    conexion=sqlite3.connect("control_de_modulo_salas.db")
+    # cursor=conexion.execute("select * from usuario WHERE numerodocumento= "+ str(documento))
+    df = pd.read_sql_query("SELECT * from prestamo", conexion)
+    print("++++++++++++++++++",len(df))
+    idprestamo=0
+    if len(df["idprestamo"])==0:
+        idprestamo=1
+    else:
+        idprestamo=df["idprestamo"].max()+1
+   
+    #nos sirve para crear el id busca los id identificas cual es el mayor y le suma uno para garantizar que el id no existe
+    
+    return idprestamo
+
+def registrar_prestamo():
+    conexion=sqlite3.connect("control_de_modulo_salas.db")
+    cursor=conexion.execute("insert into usuario (idusuario,numerodocumento,nombres,apellidos,semestre,jornada, idprograma,idtipousuario) values (?,?,?,?,?,?,?,?)", (idusuario,numerodocumento,nombres,apellidos,semestre,jornada, idprograma,idtipousuario))
+    conexion.commit()
+    conexion.close()
+    
+
+print(generar_idprestamo())
+
